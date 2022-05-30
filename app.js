97155e3d5bb737524068f3abd127d918e94483ca
Rajesh <rj.chaudhary5213@gmail.com>
2022-05-30 23:56:47 +0530

    fix silly error

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ const { join } = require('path');
 
 const MongoDBStore = require('connect-mongo');
 
-// const dbUrl=process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl=process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 // 'mongodb://localhost:27017/yelp-camp'
 
 mongoose.connect(dbUrl, {
